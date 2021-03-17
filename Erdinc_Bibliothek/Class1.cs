da67e3306bcb5118cdbd6392c474053117628eac
Salih Erding <erdinc.s03@htlwienwest.at>
2021-03-17 14:31:19 +0100

    Projektdateien hinzufügen.

---
+++ Erdinc_Bibliothek/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Erdinc_Bibliothek
+{
+	public class Class1
+	{
+	}
+}
---
+++ Erdinc_Bibliothek/DataManager.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Erdinc_Bibliothek
+{
+	public static class DataManager
+	{
+		private const string ConnectionString = "server=127.0.0.1;uid=root;database=kerer_movie";
+		public static List<Movie> GetAllMovies()
+		{
+			using (MySqlConnection connection = new MySqlConnection(ConnectionString))
+			{
+				connection.Open();
+				// TODO: more code will go here
+				connection.Close();
+
+				return null;
+			}
+		}
+		public static List<Movie> Search(string searchString) { return null; }
+	}
+}
